c69288f116ec631ba6cbd5d730852389fcdffd19
fogcity89 <kevinmtow@gmail.com>
2016-06-10 11:57:40 -0700

    added lat and longitude to front end

--- server/server.js
+++ server/server.js
@@ -33,15 +33,19 @@ app.get('/api/stationList', function(req, res) {
   });
 });
 
-app.post('/api/trainTime', function(req, res){
-  console.log('wednesday', req.body)///there is nothing on this req that is able to find
-  bart.getTrainTimes(req.body, function(data){
+app.post('/api/trainTime', function(req, res) {
+  bart.getRealTimeEstimate(req.body, function(data) {
     data = JSON.parse(data);
-    console.log('inside server.js', data)
     res.send(data);
   });
 });
 
+app.get('/api/routes', function(req, res) {
+  bart.getRoute(function(data) {
+    data = JSON.parse(data);
+    res.send(data);
+  })
+})
 
 app.listen(port, function() {
   console.log("Listening on port " + port);
