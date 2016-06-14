ed5cf6c86bd6dbec9e80e52026f3dba8526a179b
ckim16 <aero.cheewoon@gmail.com>
2016-06-13 23:02:49 -0700

    add zoom in padding feature

--- client/app/controllers/bartController.js
+++ client/app/controllers/bartController.js
@@ -36,9 +36,11 @@ angular.module('app.bartInfo', [])
                     .append('svg')
                     .attr('width', width)
                     .attr('height', height)
-                    .style('border', '1px solid black');
+                    .style('border', '1px solid black')
+                    .call(d3.behavior.zoom().on("zoom", function () {canvas.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")}))
+                    .append("g");
                     
-      var g = canvas.append('g');
+      // var g = canvas.append('g');
       var projection = d3.geo.mercator();
       projection.scale(80000).translate([100, 300]).center([-122.413756, 37.779528]);
       
@@ -55,6 +57,14 @@ angular.module('app.bartInfo', [])
               .attr("cy", function(d) { return projection([d.gtfs_longitude, d.gtfs_latitude])[1] })
               .attr("r", "2px")
               .attr("fill", "red");
+
+        canvas.selectAll('text')
+              .data(data.station)
+              .enter().append('text')
+              .attr("x", function(d) { return projection([d.gtfs_longitude, d.gtfs_latitude])[0] + 5 })
+              .attr("y", function(d) { return projection([d.gtfs_longitude, d.gtfs_latitude])[1] + 5 })
+              .attr('font-size', '8px')
+              .text(function(d) { return d.name; });
       };
       
       scope.$watch('stationList', function() {
