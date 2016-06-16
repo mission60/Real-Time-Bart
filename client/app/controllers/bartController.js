2e0d7a06b81084976124692c2572990d9bc83209
ckim16 <aero.cheewoon@gmail.com>
2016-06-15 20:14:54 -0700

    mapbox and station locked together

--- client/app/controllers/bartController.js
+++ client/app/controllers/bartController.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('app.bartInfo', [])
 .controller('bartController', ['$scope', 'Bart', function($scope, Bart) {
-  // $scope.stationList;
+  $scope.Bart = Bart;
   Bart.getRTE()
   .then(function(realTimeEstimate) {
     $scope.realTimeEstimate = realTimeEstimate;
@@ -24,51 +24,119 @@ angular.module('app.bartInfo', [])
     $scope.stationList = stationList;
     return stationList;
   });
+
+  Bart.getTrainTime()
+  .then(function(getTrainTime){
+    $scope.getTrainTime = getTrainTime;
+    return getTrainTime;
+  });
 }])
 .directive('d3Stations', [function() {
   return {
     restrict: 'EA',
     scope: false,
     link: function(scope, element) {
-      var width = 900;
-      var height = 700;
-      var canvas = d3.select(element[0])
-                    .append('svg')
-                    .attr('width', width)
-                    .attr('height', height)
-                    .style('border', '1px solid black')
-                    .call(d3.behavior.zoom().on("zoom", function () {canvas.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")}))
-                    .append("g");
+      L.mapbox.accessToken = 'pk.eyJ1Ijoic3RlbmluamEiLCJhIjoiSjg5eTMtcyJ9.g_O2emQF6X9RV69ibEsaIw';
+      var map = L.mapbox.map('map', 'mapbox.pencil').setView([37.80467476, -122.2945822], 11);
+      // var width = 900;
+      // var height = 700;
+      var canvas = d3.select(map.getPanes().overlayPane)
+                    .append('svg');
+                    // .attr('width', width)
+                    // .attr('height', height)
+                    // .style('border', '1px solid black')
+                    // .call(d3.behavior.zoom().on("zoom", function () {canvas.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")}))
+                    // .append("g");
                     
-      // var g = canvas.append('g');
-      var projection = d3.geo.mercator();
-      projection.scale(80000).translate([100, 300]).center([-122.413756, 37.779528]);
+      var g = canvas.append('g').attr('class', 'leaflet-zoom-hide');
+      // var projection = d3.geo.mercator();
+      // projection.scale(80000).translate([100, 300]).center([-122.413756, 37.779528]);
       
       scope.render = function(data) {
+        // console.log('render data', data)
         if(data === undefined) {
           return;
         }
-        canvas.selectAll('*').remove();
+        // canvas.selectAll('*').remove();
+        var dataArr = [];
+        // console.log('data', data.station)
+        data.station.forEach(function(d) {
+          // dataArr.push(new L.LatLng(d.gtfs_latitude, d.gtfs_longitude));
+          d.lat = d.gtfs_latitude;
+          d.lng = d.gtfs_longitude;
+          delete d.gtfs_latitude;
+          delete d.gtfs_longitude;
+        });
 
-        canvas.selectAll('circle')
-              .data(data.station)
-              .enter().append('circle')
-              .attr("cx", function(d) { return projection([d.gtfs_longitude, d.gtfs_latitude])[0] })
-              .attr("cy", function(d) { return projection([d.gtfs_longitude, d.gtfs_latitude])[1] })
-              .attr("r", "2px")
-              .attr("fill", "red");
+        function project(latlng) {
+          // var array = [+latlng.lat, +latlng.lng];
+          var point = map.latLngToLayerPoint(L.latLng(latlng));
+          return point;
+        }
+        var tip = d3.select('body').append('div').attr('class', 'tooltip').style('opacity', 0);
+        var station = g.selectAll('circle')
+                        .data(data.station)
+                        .enter().append('circle')
+                        .attr('r', '5px')
+                        .attr('fill', 'red')
+                        .on('mouseover', function(data) {
+                          console.log('data', data)
+                          scope.Bart.getTrainTime(data.abbr)
+                          .then(function(info) {
+                            console.log('info', info);
+                            tip.transition()
+                                .duration(200)
+                                .style('opacity', 1);
+                            tip.html(info.name + '<br/>' + 'Destination: ' + info.etd)
+                               .style("left", (event.pageX) + "px")   
+                               .style("top", (event.pageY - 28) + "px"); 
+                          });
+                        })
+                        .on('mouseout', function(d) {
+                          tip.transition()
+                              .duration(500)
+                              .style('opacity', 0);
+                        });
+        var text = g.selectAll('text')
+                    .data(data.station)
+                    .enter().append('text')
+                    .attr('font-size', '8px')
+                    .text(function(d) { return d.abbr; });
+        function update() {
+          // We need to reposition our SVG and our containing group when the map
+          // repositions via zoom or pan
+          // https://github.com/zetter/voronoi-maps/blob/master/lib/voronoi_map.js
+          var bounds = map.getBounds();
+          var topLeft = map.latLngToLayerPoint(bounds.getNorthWest())
+          var bottomRight = map.latLngToLayerPoint(bounds.getSouthEast())
+          canvas.style("width", map.getSize().x + "px")
+            .style("height", map.getSize().y + "px")
+            .style("left", topLeft.x + "px")
+            .style("top", topLeft.y + "px");
+          g.attr("transform", "translate(" + -topLeft.x + "," + -topLeft.y + ")");
 
-        canvas.selectAll('text')
-              .data(data.station)
-              .enter().append('text')
-              .attr("x", function(d) { return projection([d.gtfs_longitude, d.gtfs_latitude])[0] + 5 })
-              .attr("y", function(d) { return projection([d.gtfs_longitude, d.gtfs_latitude])[1] + 5 })
-              .attr('font-size', '8px')
-              .text(function(d) { return d.name; });
+          // We reproject our data with the updated projection from leaflet
+          station.attr({
+            cx: function(d) { return project({lat: d.lat, lng: d.lng}).x },
+            cy: function(d) { return project({lat: d.lat, lng: d.lng}).y }
+          });
+          text.attr({
+            x: function(d) { return project({lat: d.lat, lng: d.lng}).x + 5 },
+            y: function(d) { return project({lat: d.lat, lng: d.lng}).y + 8 }
+          });
+        }
+        map.on("viewreset", function() {
+          update();
+        });
+        map.on("move", update);
+
+        // render our initial visualization
+        update();
+
       };
       
       scope.$watch('stationList', function() {
-        if(scope.stationList !== undefined){
+        if(scope.stationList !== undefined) {
           scope.render(scope.stationList);
         }
       });
