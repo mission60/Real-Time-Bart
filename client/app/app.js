14d0f663063b2f4b9cbbac75e1ca90674dd6251d
ckim16 <aero.cheewoon@gmail.com>
2016-06-19 15:23:51 -0700

    mouse hover location fixed & add css files

--- client/app/app.js
+++ client/app/app.js
@@ -8,6 +8,10 @@ angular.module('app', [
 ])
 .config(function($routeProvider, $locationProvider) {
   $routeProvider
+  // .when('/', {
+  //   templateUrl: 'app/views/indexView.html',
+  //   controller: 'indexController' 
+  // })
   .when('/', {
     templateUrl: 'app/views/bartInfo.html',
     controller: 'bartController'
